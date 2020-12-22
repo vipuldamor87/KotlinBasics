ea269ad3e8fe005c0deef3ecbce0c058f7d70048
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-22 13:15:26 +0530

    git push -f origin master commit

--- app/src/main/java/com/vipuldamor87/myapplication/MainActivity.kt
+++ app/src/main/java/com/vipuldamor87/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.vipuldamor87.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    //val bt1 = findViewById<Button>(R.id.bt1)
+val max= 4;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt1.setOnClickListener {
+            var a: String = edt1.text.toString()
+            Log.i("MainActivity","this button has been clicked")
+            Toast.makeText(this,a,Toast.LENGTH_LONG).show()
+        }
+
     }
 }
