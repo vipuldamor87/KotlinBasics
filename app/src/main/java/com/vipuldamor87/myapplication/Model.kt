e4aa88fbd295fee4acdb9252b2c6203dd385e412
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-22 15:52:44 +0530

    commit

---
+++ app/src/main/java/com/vipuldamor87/myapplication/Model.kt
@@ -0,0 +1,20 @@
+package com.vipuldamor87.myapplication
+
+data class Hobby(var title:String)
+
+object Supplier {
+    val hobbies = listOf<Hobby>(
+        Hobby("walking"),
+        Hobby("running"),
+        Hobby("swimming"),
+        Hobby("asf"),
+        Hobby("walking"),
+        Hobby("running"),
+        Hobby("swimming"),
+        Hobby("asf"),
+        Hobby("walking"),
+        Hobby("running"),
+        Hobby("swimming"),
+        Hobby("asf")
+    )
+}
